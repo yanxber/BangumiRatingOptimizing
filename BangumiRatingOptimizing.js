b9a7f5a9c5f57065704617ed148fc781ba0cf172
幻度 <yanxber@users.noreply.github.com>
2018-03-18 12:35:10 +0800

    Update BangumiRatingOptimizing.js

--- BangumiRatingOptimizing.js
+++ BangumiRatingOptimizing.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        评分显示优化
-// @namespace   https://github.com/yanxber
+// @namespace   https://bgm.tv/user/yanber
 // @version     1.2
 // @description 在番剧详情页面显示评分的更多位小数以及总体标准偏差
 // @author      ruocaled , fifth , yanber
@@ -24,8 +24,10 @@ if(deviation<1.2){
 }else{
     dispute = "厨黑大战";
 }
+(function(){
 $(".global_score .number").text(mean.toFixed(4));
 $("#ChartWarpper").append("标准差： " + deviation + " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 争议度： "+ dispute);
+})();
 function calculateSD(scores, mean, n) {
     let sd = 0;
     scores.forEach(function (elem, index) {
