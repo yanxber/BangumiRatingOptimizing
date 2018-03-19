8c0273a671256415f4f900c59085ac4518246e49
幻度 <yanxber@users.noreply.github.com>
2018-03-20 02:31:34 +0800

    update v1.3

--- BangumiRatingOptimizing.user.js
+++ BangumiRatingOptimizing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         评分显示优化
 // @namespace    https://bgm.tv/user/yanber
-// @version      1.2
+// @version      1.3
 // @description  在番剧详情页面显示评分的更多位小数和总体标准偏差
 // @author       ruocaled , fifth , yanber
 // @include      /^https?:\/\/(bangumi|bgm|chii)\.(tv|in)\/subject\/.*$/
@@ -14,19 +14,23 @@ let scores = $(".horizontalChart .count").text().replace(/\)|\(/g," ").trim().sp
 let mean = scores.reduce(function(b,d,c){a+=d;return b+d*(10-c);},0)/a||0;
 let deviation = calculateSD(scores, mean, total).toFixed(4);
 let dispute = "";
-if(deviation<1.2){
-    dispute = "全员一致";
-}else if(deviation<1.4){
-    dispute = "略有分歧";
+if(deviation<1){
+    dispute = "<b style='color:Blue'>异口同声</b>";
+}else if(deviation<1.15){
+    dispute = "<b style='color:Aqua'>基本一致</b>";
+}else if(deviation<1.3){
+    dispute = "<b style='color:Green'>略有分歧</b>";
+}else if(deviation<1.45){
+    dispute = "<b style='color:Olive'>莫衷一是</b>";
 }else if(deviation<1.6){
-    dispute = "褒贬不一";
-}else if(deviation<1.8){
-    dispute = "你死我活";
+    dispute = "<b style='color:OrangeRed'>各执一词</b>";
+}else if(deviation<1.75){
+    dispute = "<b style='color:DarkRed'>你死我活</b>";
 }else{
-    dispute = "厨黑大战";
+    dispute = "<b style='color:Red'>厨黑大战</b>";
 }
 $(".global_score .number").text(mean.toFixed(4));
-$("#ChartWarpper").append("标准差： " + deviation + " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 可信度： " + dispute);
+$("#ChartWarpper").append("标准差： " + deviation + " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 争议度： " + dispute);
 function calculateSD(scores, mean, n) {
     let sd = 0;
     scores.forEach(function (elem, index) {
